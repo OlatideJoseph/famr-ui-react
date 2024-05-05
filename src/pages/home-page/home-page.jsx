fbdb3cbfec1435dc7888e2fa565e6aa4c1972895
Olatide <olatidejoseph17@gmail.com>
2024-05-06 00:51:57 +0100

    Pages declaration structure & home-page creation

---
+++ src/pages/home-page/home-page.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './home-page.css'
+
+const HomePage = (props) => {
+  return (
+    <div
+      className='home-section'
+    >
+      <h1>Home Page !</h1>
+    </div>
+  )
+}
