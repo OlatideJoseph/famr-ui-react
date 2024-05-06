294e8b00adfbc30d47b1c9ad4d92917b7bef7597
Olatide <olatidejoseph17@gmail.com>
2024-05-06 01:38:55 +0100

    home page view seperator

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -6,7 +6,20 @@ const HomePage = (props) => {
     <div
       className='home-section'
     >
-      <h1>Home Page !</h1>
+      <div className='container'>
+        <div className='section1'>
+          <h1>
+            Welcome To Your Path Of Excellence !
+          </h1>
+          <p>
+            Your Pursuit Of Knowledge, Your Dream Career, And Your Aspirations - It All
+            Begins Here, We're Not Just App; We're Your Trusted Companion On This Exciting Journery
+            On University Admission.
+          </p>
+        </div>
+        <div className='section2'>
+        </div>
+      </div>
     </div>
   )
 }
