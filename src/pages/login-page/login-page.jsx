e3084b1097641bfb4e1dcdd1aa07e20cccf321cf
Olatide <olatidejoseph17@gmail.com>
2024-05-10 04:09:32 +0100

    react

--- src/pages/login-page/login-page.jsx
+++ src/pages/login-page/login-page.jsx
@@ -4,11 +4,23 @@ import BootStrapButton from '../../components/buttons/bootstrap-button'
 
 const LoginPage = () => {
   document.title = "Log In"
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const handleUsername = (event) => {
+    setUsername(event.target.value)
+  }
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(event)
     console.log("submitted")
   }
+  /*Login Page Side Effect*/
+  React.useEffect(()=>{
+    console.log('side effect')
+  }, [username, password])
 	return (
     <div
       className='login-page'
@@ -21,11 +33,11 @@ const LoginPage = () => {
             <div className="row">
               <div className="col">
                 <label className="form-label" htmlFor="username">Username: </label>
-                <input className="form-control" id="username" name="username" required="" type="text" defaultValue=""/>
+                <input className="form-control" onChange={handleUsername} id="username" name="username" required="" type="text"/>
               </div>            
               <div className="col">
                 <label className="form-label" htmlFor="password">Password: </label>
-                <input className="form-control" id="password" name="password" required="" type="password" defaultValue=""/>
+                <input className="form-control" onChange={handlePassword} id="password" name="password" required="" type="password"/>
               </div>
             </div>
             <div className="row">
