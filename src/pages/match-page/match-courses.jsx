b76728d0fcafc9942081b9b3e8e3c7afda40bbb8
Olatide <olatidejoseph17@gmail.com>
2024-05-22 02:15:23 +0100

    API Requests Data For Match

--- src/pages/match-page/match-courses.jsx
+++ src/pages/match-page/match-courses.jsx
@@ -1,9 +1,78 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 import './match-courses.css'
 
 const MatchCourses = ({ user, defaultUrl }) => {
   document.title = 'Match Course'
+  const [formData, formDataDispatcher] = React.useReducer((state, action)=>{
+    switch (action.type){
+      case ('SET_SUBJECTS'):
+        return {
+          ...state,
+          subjects : action.payload.subjects
+        }
+      case ('SET_COURSES'):
+        return {
+          ...state,
+          courses : action.payload.courses
+        }
+      case ('SET_GRADE_POINT'):
+        return {
+          ...state,
+          gradePoint : action.payload.gradePoint
+        }
+      default:
+        throw new Error()
+    }
+  }, { subjects: [], courses : [], gradePoint: [] })
+  React.useEffect(()=>{
+    let token = localStorage.getItem('refresh_token')
+    let subjects = localStorage.getItem('subjects')
+    let courses = localStorage.getItem('formCourses')
+    let gradePoint = localStorage.getItem('gradePoint')
+
+    if (token && !subjects && !courses && !gradePoint){
+      axios.get(`${defaultUrl}ajax/v1.0/get-course-data/`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(resp => resp.data )
+      .then(data => {
+        console.log(data)
+        formDataDispatcher({type: 'SET_COURSES', payload: {courses: data}})
+        localStorage.setItem('formCourses', JSON.stringify(data))
+      })
+      /*---------------------------------------------------*/
+      axios.get(`${defaultUrl}ajax/v1.0/get-subject-data/`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(resp => resp.data )
+      .then(data => {
+        formDataDispatcher({type:'SET_SUBJECTS', payload: { subjects: data}})
+        localStorage.setItem('subjects', JSON.stringify(data))
+      })
+      /*---------------------------------------------------*/
+      axios.get(`${defaultUrl}ajax/v1.0/get-grade-and-point/`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(resp => resp.data )
+      .then(data => {
+        formDataDispatcher({type: 'SET_GRADE_POINT', payload: {gradePoint: data}})
+        localStorage.setItem('gradePoint', JSON.stringify(data))
+      })
+      /*---------------------------------------------------*/
+    }else{
+      formDataDispatcher({type:'SET_SUBJECTS', payload: {subjects: JSON.parse(subjects)}})
+      formDataDispatcher({type:'SET_COURSES', payload: {courses: JSON.parse(courses)}})
+      formDataDispatcher({type:'SET_GRADE_POINT', payload: {gradePoint:JSON.parse(gradePoint)}})
+    }
+  }, [])
 	return (
     <div className='match-course'>
       <div className='container-fluid col-md-8'>
@@ -22,25 +91,46 @@ const MatchCourses = ({ user, defaultUrl }) => {
               <div className="col">
                 <label className="form-label " htmlFor="course_name">Choose Your Course</label>
                 <select className="form-select form-select-sm" id="course_name" name="course_name" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.courses.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div> 
               <div className="col">
                 <label className="form-label" htmlFor="jamb_score">Jamb Score</label>
-                <input className="form-control" id="jamb_score" max="400" min="180" type="number" name="jamb_score"/>
+                <input className="form-control" id="jamb_score" max="400" min="180" type="number" name="jamb_score" defaultValue='180'/>
               </div>           
             </div>
             <div className="row">
               <div className="col">
                 <label className="form-label" htmlFor="field1">Subject 1</label>
-                <select className=" form-select form-select-sm" id="field1" name="field1" required="">
-                  
+                <select className="form-select form-select-sm" id="field1" name="field1" required="">
+                  <option value="">-------------</option>
+                  {
+                    formData.subjects.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div>
               <div className="col">
                 <label className="form-label" htmlFor="grade_1">Grade *</label>
                 <select className="form-select form-select-sm" id="grade_1" name="grade_1" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.gradePoint.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div>
             </div>
@@ -48,13 +138,27 @@ const MatchCourses = ({ user, defaultUrl }) => {
               <div className="col">
                 <label className="form-label" htmlFor="field2">Subject 2</label>
                 <select className="form-select form-select-sm" id="field2" name="field2" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.subjects.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>    
               </div>
               <div className="col">
                 <label className="form-label" htmlFor="grade_2">Grade *</label>
                 <select className="form-select form-select-sm" id="grade_2" name="grade_2" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.gradePoint.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div>
             </div>
@@ -62,13 +166,27 @@ const MatchCourses = ({ user, defaultUrl }) => {
               <div className="col">
                 <label className="form-label" htmlFor="field3">Subject 3</label>
                 <select className="form-select form-select-sm" id="field3" name="field3" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.subjects.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div>
               <div className="col">
                 <label className="form-label" htmlFor="grade_3">Grade *</label>
                 <select className="form-select-sm form-select" id="grade_3" name="grade_3" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.gradePoint.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div>
             </div>
@@ -76,13 +194,27 @@ const MatchCourses = ({ user, defaultUrl }) => {
               <div className="col">
                 <label className="form-label" htmlFor="field4">Subject 4</label>
                 <select className="form-select form-select-sm" id="field4" name="field4" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.subjects.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>  
               </div>
               <div className="col">
                 <label className="form-label" htmlFor="grade_4">Grade *</label>
                 <select className="form-select form-select-sm" id="grade_4" name="grade_4" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.gradePoint.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div>
             </div>
@@ -90,13 +222,27 @@ const MatchCourses = ({ user, defaultUrl }) => {
               <div className="col">
                 <label className="form-label" htmlFor="field5">Subject 5</label>
                 <select className="form-select form-select-sm" id="field5" name="field5" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.subjects.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>  
               </div>
               <div className="col">
                 <label className="form-label" htmlFor="grade_5">Grade *</label>
                 <select className="form-select form-select-sm" id="grade_5" name="grade_5" required="">
-                  
+                  <option value="">-------------</option>
+                  {
+                    formData.gradePoint.map((v)=>(
+                      <option key={v[0]} value={v[0]}>
+                        {v[1]}
+                      </option>
+                    ))
+                  }
                 </select>
               </div>
             </div>
